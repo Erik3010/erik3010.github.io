bde4d9a77c00f44f7265df22b0983c2dd13bc6c7
Erik <erikhau3010@gmail.com>
2021-10-10 00:04:46 +0700

    feat: add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
