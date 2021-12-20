12afe3067c4e8d4081893ed3497d83c8d0138b52
Erik <erikhau3010@gmail.com>
2021-12-20 20:46:19 +0700

    fix: stuck gradient effect when mouse move too fast

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 import "./style.css";
 
 const portfolioCards = document.querySelectorAll(".portfolio-card");
-
-const tolerance = 12;
 portfolioCards.forEach((card) => {
   card.addEventListener("mousemove", ({ clientY, clientX }) => {
-    const { top, left, width, height } = card.getBoundingClientRect();
+    const { top, left } = card.getBoundingClientRect();
     const x = clientX - left;
     const y = clientY - top;
 
-    if (
-      y - tolerance > 0 &&
-      y < height - tolerance &&
-      x - tolerance > 0 &&
-      x < width - tolerance
-    ) {
-      card.style.backgroundImage = `radial-gradient(circle at ${x}px ${y}px, rgb(79, 20, 204) 0%, rgba(79, 20, 204, 0) calc(0% + 150px))`;
-    } else {
-      card.style.backgroundImage = "none";
-    }
+    card.style.backgroundImage = `radial-gradient(circle at ${x}px ${y}px, rgb(79, 20, 204) 0%, rgba(79, 20, 204, 0) calc(0% + 150px))`;
   });
+
+  card.addEventListener(
+    "mouseout",
+    () => (card.style.backgroundImage = "none")
+  );
 });
